8d86dc7437d38e45c598b42abe8042bf8c739f56
Sarthak Rawat <130914380+sarthakrawat498@users.noreply.github.com>
2024-08-25 08:53:00 +0000

    learnt about variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId  = 144553
+let accountEmail = "sarthak@google.com"
+var accountPassword = "12345"
+accountCIty = "Delhi" 
+let accountState;
+
+//accountId = 2 not allowed 
+/*
+prefer not to use var bcz of issue in block scope and functional scope 
+*/
+
+accountEmail="hc@hc.com"
+accountPassword="21212121"
+accountCIty="bengaluru"
+
+
+
+console.log(accountId);
+console.table([accountEmail, accountId, accountPassword,accountCIty,accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict "; //treat all code as newer version 
+
+// alert(3+3) does not work in node js only browser 
+
+console.log(3+3)
+let name="sarthak"
+let age = 5
+let isloggedin = false 
+
+console.table([name , age , isloggedin])
+
+//undefined means value is not given till now 
+//null means a special type which means it is empty , standalone value 
+//symbol = unique
+
+console.log(typeof "sarthak");
+console.log(typeof null); //type is object 
+console.log(typeof undefined); //type is undefined only 
+
+
