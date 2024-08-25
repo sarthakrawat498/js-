a466edc42c045572df4fe136d84e5a94fa31e4db
Sarthak Rawat <130914380+sarthakrawat498@users.noreply.github.com>
2024-08-25 16:51:10 +0530

    04_comparison.js
    about comparison and what to use and what not to

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,30 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2" > 1 );
+// console.log("02" > 1);
+
+console.log(null > 0 );
+console.log(null == 0 );
+console.log(null >= 0 );
+
+console.log(undefined>0);
+
+console.log(undefined == 0);
+console.log(undefined < 0);
+
+// undefined gives false on every comaparison 
+//=== strict check everything , datatype and value
+//avoid them  
+
+console.log("2"==2);
+console.log("2"===2);
+
+
+
+
+
+
