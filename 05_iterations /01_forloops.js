d547b0b49eea2455f3cf04b8fb00645a5ed18056
Sarthak Rawat <130914380+sarthakrawat498@users.noreply.github.com>
2024-08-28 23:59:31 +0530

    Create 01_forloops.js

---
+++ 05_iterations /01_forloops.js
@@ -0,0 +1,53 @@
+//for 
+
+
+// for (let index = 0; index <= 10; index++) {
+//     const element = index;
+//     if (element==5){
+//         console.log("5 is best number ");
+        
+//     }
+//     //console.log(index);   
+// }
+// for (let i = 1; i <= 10; i++) {
+//     //console.log(`outer loop value ${i}`);
+    
+//     for (let j = 1; j <=10; j++) {
+//        // console.log(`inner loop value ${j} amd inner loop ${i}`);
+//         console.log(i+ '*' + j + ' = ' + i*j );
+        
+
+//     }
+    
+// }
+
+// let myArray = ["flash" , "batman" , "deadpool"]
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element);
+    
+    
+// }
+
+
+
+// break and continue 
+// for (let index = 0; index <=20; index++) {
+//     if (index==5){
+//         console.log("5 Detected");
+//         break
+        
+//     }
+// console.log("value of index is " + index);
+ 
+// }
+
+for (let index = 0; index <=20; index++) {
+    if (index==5){
+        console.log("5 Detected");
+        continue
+        
+    }
+console.log("value of index is " + index);
+ 
+}
